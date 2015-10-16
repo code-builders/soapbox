047882e625fc73a9654e54d09f5b03b99e7bc93a
Chris Hintz <chrishintz42@gmail.com>
2015-10-16 14:41:50 -0700

    Posts formatted feed

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'bubble'
+require "colorize"
 
 username = ""
 
@@ -6,8 +7,22 @@ a = 0
 while a < 1
   puts "What is your username?"
     username = gets.chomp.downcase.to_s
-    user = SoapBox.new(username: username)
+    user = Bubble.new(username: username)
     a += 1
+end
+
+body = ""
+
+d = Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
+  d.each do |file|
+    r = File.open(file)
+    answers = {
+      body: File.basename(file),
+      username: r.read,
+      created_at: r.birthtime.strftime("%b %e, %l:%M %p")
+    }
+    nb = Bubble.new(answers)
+    puts nb.formatted_string
 end
 
 body = ""
@@ -18,14 +33,18 @@ while body.downcase != "exit"
   body = gets.chomp
 
   if body.downcase != "exit"
-    t = SoapBox.new(created_at: Time.now)
-    body1 = SoapBox.new(username: username, body: body)
+    t = Bubble.new(created_at: Time.now)
+    body1 = Bubble.new(username: username, body: body)
     body2 = body1.body + " \n" + t.created_at.strftime("%A, %d %b %Y %l:%M %p").to_s
     bubbles.push(body2)
-    puts body1.write_file
+    body1.write_file
   end
-
-puts user.username.capitalize
-puts bubbles
-
 end
+
+# puts user.username.capitalize.blue
+# answers = {
+#   username: files.open
+# }
+#
+#
+# puts bubbles.formatted_string
--- bubble.rb
+++ bubble.rb
@@ -1,18 +1,79 @@
-class SoapBox
+require "colorize"
+
+class Bubble
 
   attr_accessor :username,
                 :body,
                 :created_at
+                :files
 
   def initialize(attrs={})
     @username     = attrs[:username]
     @body         = attrs[:body]
     @created_at   = attrs[:created_at]
+    @files        = attrs[:files]
+  end
+
+  def formatted_string
+      "#{username.light_red}:  #{body}
+         #{created_at}
+      ------------------------------------------------------------------------"
+  end
+
+
+  def self.read_all
+    Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
   end
 
   def write_file
-    File.open("/Users/Chris/Code/class_projects/soapbox/trees/#{@body}.txt", 'w') do |file|
-      file << "#{@username.capitalize}" 
+    File.open("/Users/Chris/Dropbox/SoapBox/#{@body}", 'w') do |file|
+      file << "#{@username.capitalize}"
     end
   end
+
+  def read_files
+     files = Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
+     puts files
+  end
 end
+
+# Daniel example below
+# for usernames
+# all_bubble_handles = Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
+# all_bubble_handles.each do |file|
+#   r = File.read(file)
+#   username = r.read
+#   created_at = r.birthtime
+
+# feed_bubble_hash = { body: , username: , created_at: }
+# for the body... File.basename(element)
+
+
+# will need to sort by the time stamps (.timebirth)... format using the website from Wednesday to format date/time
+
+# everything.each do |element|
+#   the file = File.open(element)
+
+# answers = {
+# username: the_file.read,
+# body: File.basename(element),
+# created_at:
+#}
+
+# nb = Bubble.new(answers)
+# puts nb.formatted_string  #this would be in bubble
+
+# -------------------------------------------------------
+# time_sorted_feed = entire_feed.sort_by {|x| File.birthtime(x)}.reverse
+
+# entire_feed.sort_by do |x|
+  # File.birthtime(x)
+# end
+
+# time_sorted_feed.each do |file|
+#   single_feed_file = File.open(file)
+#   conents = {
+#     body: File.basename(single_feed_file),
+#     username: File.read(single_feed_file)
+#     created_at: single_feed_file.birthtime
+#   }
