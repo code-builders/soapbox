6a423e84af5d8fbf15c06af7fd0ed0e83cf0650a
Chris Hintz <chrishintz42@gmail.com>
2015-10-19 08:52:10 -0700

    login user update

--- app.rb
+++ app.rb
@@ -1,20 +1,33 @@
 require_relative 'bubble'
 require "colorize"
 
-username = ""
 
-a = 0
-while a < 1
-  puts "What is your username?"
+File.open("/Users/Chris/Code/class_projects/soapbox/soapbox/.soapbox", "w") do |file|
+  file << "@chris"
+end
+
+login_prompt = ""
+
+file = File.open("/Users/Chris/Code/class_projects/soapbox/soapbox/.soapbox")
+username_files = file.read
+puts "Continue as #{username_files} or press [n] to login as a different user."
+login_prompt = gets.chomp.downcase
+  if login_prompt == "n"
+    puts "What is your username?"
     username = gets.chomp.downcase.to_s
     user = Bubble.new(username: username)
-    a += 1
-end
+    user.save_user
+  else
+    username = "@chris"
+    user = Bubble.new(username: username)
+    user.save_user
+  end
 
 body = ""
 
-d = Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
-  d.each do |file|
+def entire_feed
+  d = Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
+  d.map do |file|
     r = File.open(file)
     answers = {
       body: File.basename(file),
@@ -22,29 +35,36 @@ d = Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
       created_at: r.birthtime.strftime("%b %e, %l:%M %p")
     }
     nb = Bubble.new(answers)
-    puts nb.formatted_string
+  end
+end
+
+def sorted_feed
+  newest_last_feed = entire_feed.sort_by do |x|
+    x.created_at
+  end
+  newest_last_feed.reverse.first(10)
 end
 
 body = ""
 bubbles = []
+options = ""
 
-while body.downcase != "exit"
-  puts "What would you like to Bubble? (Type \"exit\" to end program)"
+while options != "e"
+  puts "What would you like to Bubble?"      # Add the sort_by method
   body = gets.chomp
-
-  if body.downcase != "exit"
-    t = Bubble.new(created_at: Time.now)
-    body1 = Bubble.new(username: username, body: body)
-    body2 = body1.body + " \n" + t.created_at.strftime("%A, %d %b %Y %l:%M %p").to_s
-    bubbles.push(body2)
-    body1.write_file
+  my_bubble = Bubble.new(username: username, body: body, created_at: Time.now)
+  bubbles.push(my_bubble)
+  puts "would you like to post [p], refresh your feed [r], or exit [e]?"
+  options = gets.chomp
+  if options.downcase == "p"
+    my_bubble.save
+  elsif options.downcase == "e"
+    break
+  else
+    sorted_feed.each do |feed|
+      puts feed.formatted_string
+    end
   end
 end
 
-# puts user.username.capitalize.blue
-# answers = {
-#   username: files.open
-# }
-#
-#
-# puts bubbles.formatted_string
+puts "Good-bye"
--- bubble.rb
+++ bubble.rb
@@ -1,17 +1,14 @@
 require "colorize"
-
 class Bubble
 
   attr_accessor :username,
                 :body,
                 :created_at
-                :files
 
   def initialize(attrs={})
     @username     = attrs[:username]
     @body         = attrs[:body]
     @created_at   = attrs[:created_at]
-    @files        = attrs[:files]
   end
 
   def formatted_string
@@ -20,60 +17,15 @@ class Bubble
       ------------------------------------------------------------------------"
   end
 
-
-  def self.read_all
-    Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
-  end
-
-  def write_file
+  def save
     File.open("/Users/Chris/Dropbox/SoapBox/#{@body}", 'w') do |file|
-      file << "#{@username.capitalize}"
+      file << "#{@username}"
     end
   end
 
-  def read_files
-     files = Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
-     puts files
+  def save_user
+    File.open("/Users/Chris/Code/class_projects/soapbox/soapbox/.soapbox", 'w') do |file|
+      file << "#{@username}"
+    end
   end
 end
-
-# Daniel example below
-# for usernames
-# all_bubble_handles = Dir.glob("/Users/Chris/Dropbox/SoapBox/*")
-# all_bubble_handles.each do |file|
-#   r = File.read(file)
-#   username = r.read
-#   created_at = r.birthtime
-
-# feed_bubble_hash = { body: , username: , created_at: }
-# for the body... File.basename(element)
-
-
-# will need to sort by the time stamps (.timebirth)... format using the website from Wednesday to format date/time
-
-# everything.each do |element|
-#   the file = File.open(element)
-
-# answers = {
-# username: the_file.read,
-# body: File.basename(element),
-# created_at:
-#}
-
-# nb = Bubble.new(answers)
-# puts nb.formatted_string  #this would be in bubble
-
-# -------------------------------------------------------
-# time_sorted_feed = entire_feed.sort_by {|x| File.birthtime(x)}.reverse
-
-# entire_feed.sort_by do |x|
-  # File.birthtime(x)
-# end
-
-# time_sorted_feed.each do |file|
-#   single_feed_file = File.open(file)
-#   conents = {
-#     body: File.basename(single_feed_file),
-#     username: File.read(single_feed_file)
-#     created_at: single_feed_file.birthtime
-#   }
